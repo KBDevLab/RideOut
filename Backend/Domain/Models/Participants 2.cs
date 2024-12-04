0cc73c323e89212342fe9c3b25a8efe1847c6d6b
Kyran Bryant <kb@kyrans-mbp.mynetworksettings.com>
2024-12-04 13:13:22 -0800

    catching up on this branch, essentially a backup/recovery.

---
+++ Backend/Domain/Models/Participants 2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RideOut.Domain.Models;
+
+public partial class Participants
+{
+    public int Participantid { get; set; }
+
+    public int Rideoutid { get; set; }
+
+    public int Userid { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public DateTime? Joinedat { get; set; }
+
+    public virtual Rideouts Rideout { get; set; } = null!;
+
+    public virtual Users User { get; set; } = null!;
+}
